87ed3648c6eac48de3b7ad4c1acea74409bafc98
Cole Peppis <cpeppis@occs.cs.oberlin.edu>
2014-11-01 15:15:19 -0400

    FIXED SHIT YAY

--- jabber.py
+++ jabber.py
@@ -5,9 +5,9 @@
 
 from predres import *
 
-b = [[('~shark','X'),('certainWFO','X')],[('~fish','Y'),('canDanceM','Y'),('contemptible','Y')],[('~fish','Z'),('has3RT','Z'),('~certainWFO','Z')],[('~fish','W'),('shark','W'),('~kindToC','W')],[('~fish','V'),('~heavy','V'),('~canDanceM','V')],[('~fish','U'),('~has3RT','U'),('~contemptible','U')],[('fish','t')],[('heavy','t')],[('kindToC','t')]]
+b = [[('~shark','X'),('certainWFO','X')],[('~fish','Y'),('canDanceM','Y'),('contemptible','Y')],[('~fish','Z'),('has3RT','Z'),('~certainWFO','Z')],[('~fish','W'),('shark','W'),('kindToC','W')],[('~fish','V'),('~heavy','V'),('~canDanceM','V')],[('~fish','U'),('~has3RT','U'),('~contemptible','U')],[('fish','t')],[('heavy','t')],[('~kindToC','t')]]
 # since the fish(X) occurs in all of the clauses and isn't necessary for the resolution, we removed it:
-c = [[('~shark','X'),('certainWFO','X')],[('canDanceM','Y'),('contemptible','Y')],[('has3RT','Z'),('~certainWFO','Z')],[('shark','W'),('~kindToC','W')],[('~heavy','V'),('~canDanceM','V')],[('~has3RT','U'),('~contemptible','U')],[('heavy','t')],[('kindToC','t')]]
+c = [[('~shark','X'),('certainWFO','X')],[('canDanceM','Y'),('contemptible','Y')],[('has3RT','Z'),('~certainWFO','Z')],[('shark','W'),('kindToC','W')],[('~heavy','V'),('~canDanceM','V')],[('~has3RT','U'),('~contemptible','U')],[('heavy','t')],[('~kindToC','t')]]
 
 ans = resolveAll(c)
 l = next(ans)
